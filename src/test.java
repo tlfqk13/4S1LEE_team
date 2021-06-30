3d1233d34d209a67171e731754f5965e4c34b34d
user <user@LAPTOP-1VG8E5NB>
2021-06-30 18:58:02 +0900

    제발

--- src/test.java
+++ src/test.java
@@ -1,12 +1,12 @@
 
 public class test {
-		//test 입니다
-		//testst//test 입니다
-	//testst//test 입니다
-	//testst//test 입니다
-	//testst//test 입니다
+		//test �엯�땲�떎
+		//testst//test �엯�땲�떎
+	//testst//test �엯�땲�떎
+	//testst//test �엯�땲�떎
+	//testst//test �엯�땲�떎
 	//testst
 	//tetet
-	
+	// 심유진 ㅜ머
 	
 }
